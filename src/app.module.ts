47ea8ac4f973713de3a84f3aa9d0b5c7f447bdfa
iheboueslati909 <hooba500@gmail.com>
2024-09-06 15:02:26 +0100

    auth integration ( untested )

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-//import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/auth-workshop'),
+    MongooseModule.forRoot('mongodb://localhost:27017/user'),
     UserModule,
-//    AuthModule,
+    AuthModule,
     ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true,
-    })],
+    }),
+  AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,13 +1,12 @@
-import { Prop, Schema, SchemaFactory }
-from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
 @Schema()
 export class User {
-@Prop()
-name: string;
-@Prop({ unique: true })
-email: string;
-
-@Prop()
-password: string;
+    @Prop()
+    name: string;
+    @Prop({ unique: true })
+    email: string;
+    @Prop()
+    password: string;
 }
 export const UserSchema = SchemaFactory.createForClass(User);
