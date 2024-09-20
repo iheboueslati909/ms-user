04d7f297bf197c4792922e36586681df65db20ea
iheboueslati909 <hooba500@gmail.com>
2024-09-20 19:58:55 +0100

    user validation*

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+    @IsNotEmpty({ message: 'Username is required' })
     username: string;
+
+    @IsEmail({}, { message: 'Invalid email address' })
     email: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
     password: string;
-    }
+}
