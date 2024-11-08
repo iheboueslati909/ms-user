4258a4e014918d62dc12dec71a68c2b0083ac262
iheboueslati909 <hooba500@gmail.com>
2024-11-08 14:21:04 +0100

    Reapply ".."
    This reverts commit 5ecc66751de2c6662756c82d46756c9eea3dfacc.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,9 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from './entities/user.entity';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    AuthModule,
     MongooseModule.forFeature([{
       name: User.name, schema:
         UserSchema
